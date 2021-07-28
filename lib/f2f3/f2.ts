ef914751bd9042f536a088945f847d00bb866247
TomTom <thetomtom3901business@gmail.com>
2021-07-28 18:50:10 +0100

    remove unnecessary f2f3 variables

--- lib/f2f3/f2.ts
+++ lib/f2f3/f2.ts
@@ -3,10 +3,9 @@ import { RacesResponse } from './types'
 import { mapResponseToData } from './mapper'
 
 const f2Client = client(process.env.F2_KEY)
-const RACES_ENDPOINT = `/races?website=f2`
 
 export const getRaces = async () => {
-  const { data } = await f2Client.get<RacesResponse>(RACES_ENDPOINT)
+  const { data } = await f2Client.get<RacesResponse>(`/races?website=f2`)
 
   return mapResponseToData(data)
 }
--- lib/f2f3/f3.ts
+++ lib/f2f3/f3.ts
@@ -3,10 +3,9 @@ import { RacesResponse } from './types'
 import { mapResponseToData } from './mapper'
 
 const f3Client = client(process.env.F3_KEY)
-const RACES_ENDPOINT = `/races?website=f3`
 
 export const getRaces = async () => {
-  const { data } = await f3Client.get<RacesResponse>(RACES_ENDPOINT)
+  const { data } = await f3Client.get<RacesResponse>(`/races?website=f3`)
 
   return mapResponseToData(data)
 }
