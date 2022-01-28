ea518bf4bc3e53bf1d3a07870ebebeb9f6b41a28
appsbytom <appsbytomas@gmail.com>
2022-01-28 00:04:50 +0000

    enable reactStrictMode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withTM = require('next-transpile-modules')(['react-timezone-select'])
 
 module.exports = withTM({
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  reactStrictMode: true
 })
