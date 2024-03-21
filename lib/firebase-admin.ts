24efa82c6121c6adffe9e5b4b7267582b4d82dfb
appsbytom <appsbytomas@gmail.com>
2024-03-21 00:16:41 +0000

    fix admin credentials

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,7 @@
-import { applicationDefault, getApps, initializeApp } from 'firebase-admin/app'
+import { getApps, initializeApp, cert } from 'firebase-admin/app'
 import { getMessaging } from 'firebase-admin/messaging'
 
-if (!getApps().length) initializeApp({ credential: applicationDefault() })
+if (!getApps().length) initializeApp({ credential: cert(JSON.parse(process.env.FIREBASE_CREDENTIALS)) })
 
 export const messaging = getMessaging()
 
