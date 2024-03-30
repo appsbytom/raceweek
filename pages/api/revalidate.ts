0c906e4285d6f36cfc3d82dc4b5ec058a38dac4b
appsbytom <appsbytomas@gmail.com>
2024-03-30 21:08:54 +0000

    move reminders to revalidation

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,15 +1,44 @@
+import prisma from '@/lib/prisma'
+import SERIES_CONFIG from '@/series/config'
+import { getAllEvents } from '@/series/fetcher-config'
+import dayjs from 'dayjs'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.headers.token !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: 'invalid token' });
+    return res.status(401).json({ message: 'invalid token' })
   }
 
   try {
-    await res.revalidate('/');
-    return res.json({ revalidated: true });
+    const [_, events] = await Promise.all([res.revalidate('/'), getAllEvents()])
+    await prisma.$transaction(events
+      .filter(event => !event.provisional)
+      .flatMap(event => event.sessions
+        .filter(session => !session.unconfirmed && dayjs(session.startTime).isAfter(dayjs()))
+        .map(session => {
+          const id = `${event.series}-${session.id}`
+          const title = `${SERIES_CONFIG[event.series].name}: ${event.name}`
+          const body = `${session.name} starts in a few minutes`
+          const scheduledFor = dayjs(session.startTime).subtract(5, 'minutes').unix()
+          return prisma.sessionReminder.upsert({
+            where: { id },
+            create: {
+              id,
+              title,
+              body,
+              scheduledFor,
+              topic: `${event.series}-${session.type}`
+            },
+            update: {
+              title,
+              body,
+              scheduledFor
+            }
+          })
+        })))
+    return res.json({ revalidated: true })
   } catch (err) {
-    return res.status(500).json({ message: 'server error' });
+    return res.status(500).json({ message: 'server error' })
   }
 }
 
--- scripts/update-data.ts
+++ scripts/update-data.ts
@@ -1,35 +1,10 @@
 import prisma from '@/lib/prisma'
-import SERIES_CONFIG from '@/series/config'
-import { getAllEvents } from '@/series/fetcher-config'
 import axios from 'axios'
-import dayjs from 'dayjs'
 
 const CONSOLE_WRAPPER = '---------- UPDATE DATA ----------'
 
 const main = async () => {
   console.log(CONSOLE_WRAPPER)
-
-  await prisma.$transaction((await getAllEvents()).filter(event => !event.provisional).flatMap(event => event.sessions.filter(session => !session.unconfirmed && dayjs(session.startTime).isAfter(dayjs())).map(session => {
-    const id = `${event.series}-${session.id}`
-    const title = `${session.name} starts in 5 minutes`
-    const body = `${SERIES_CONFIG[event.series].name}: ${event.name}`
-    const scheduledFor = dayjs(session.startTime).subtract(5, 'minutes').unix()
-    return prisma.sessionReminder.upsert({
-      where: { id },
-      create: {
-        id,
-        title,
-        body,
-        scheduledFor,
-        topic: `${event.series}-${session.type}`
-      },
-      update: {
-        title,
-        body,
-        scheduledFor
-      }
-    })
-  })))
 
   try {
     await axios.get(`${process.env.APP_URL}/api/revalidate`, { headers: { token: process.env.TOKEN }})
