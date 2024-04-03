83e25a82c0b65d9d45fb93d8cff93d9c8153b83f
appsbytom <appsbytomas@gmail.com>
2024-04-03 22:31:46 +0100

    simplify update script

--- scripts/update-data.ts
+++ scripts/update-data.ts
@@ -1,26 +1,10 @@
-import prisma from '@/lib/prisma'
 import axios from 'axios'
 
-const CONSOLE_WRAPPER = '---------- UPDATE DATA ----------'
-
-const main = async () => {
-  console.log(CONSOLE_WRAPPER)
-
+(async () => {
   try {
-    await axios.get(`${process.env.APP_URL}/api/revalidate`, { headers: { token: process.env.TOKEN }})
+    await axios.get(`${process.env.APP_URL}/api/revalidate`, { headers: { token: process.env.TOKEN } })
     console.log('Revalidated!')
   } catch (e) {
-    console.log('Failed to revalidate:', e.response.data.message)
+    throw new Error(`Failed to revalidate: ${e.response.data.message}`)
   }
-
-  console.log(CONSOLE_WRAPPER)
-}
-
-main()
-  .catch(reason => {
-    console.error(reason)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+})()
