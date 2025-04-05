0dab4a3ec1f5712719b568d6f896b8dabb325f27
appsbytom <appsbytomas@gmail.com>
2025-04-05 21:27:18 +0100

    add reminders error handling

--- pages/api/reminders.ts
+++ pages/api/reminders.ts
@@ -5,32 +5,37 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const now = dayjs()
-  const reminders = await prisma.sessionReminder.findMany({
-    where: {
-      scheduledFor: {
-        lte: now.unix(),
-        gte: now.subtract(5, 'minutes').unix()
+  try {
+    const reminders = await prisma.sessionReminder.findMany({
+      where: {
+        scheduledFor: {
+          lte: now.unix(),
+          gte: now.subtract(5, 'minutes').unix()
+        }
       }
-    }
-  })
+    })
 
-  if (reminders.length === 0) {
-    return res.send('No reminders to send')
+    if (reminders.length === 0) {
+      return res.send('No reminders to send')
+    }
+    await Promise.all([
+      messaging.sendEach(reminders.map(reminder => ({
+        notification: { title: reminder.title, body: reminder.body },
+        topic: reminder.topic,
+        webpush: {
+          headers: {
+            Urgency: 'high',
+            TTL: '300'
+          }
+        }
+      }))),
+      prisma.sessionReminder.deleteMany({ where: { id: { in: reminders.map(reminder => reminder.id) } } })
+    ])
+    res.status(200).end()
+  } catch (e) {
+    console.error(`Failed to send reminders: ${e}`)
+    res.status(500).send(e.message)
   }
-  await Promise.all([
-    messaging.sendEach(reminders.map(reminder => ({
-      notification: { title: reminder.title, body: reminder.body },
-      topic: reminder.topic,
-      webpush: {
-        headers: {
-          Urgency: 'high',
-          TTL: '300'
-        },
-      },
-    }))),
-    prisma.sessionReminder.deleteMany({ where: { id: { in: reminders.map(reminder => reminder.id) } } })
-  ])
-  res.status(200).end()
 }
 
 export default handler
